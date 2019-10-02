0e9cb6978c4bbfca037f09ef531a0595c897be72
Miles Eastwood <meastwood@spartaglobal.com>
2019-10-02 10:51:23 +0100

    Used SQL language in Pycharm. Utilised for loops and while loops to iterate though information

--- db_connect.py
+++ db_connect.py
@@ -8,9 +8,48 @@ password = 'Passw0rd2018'
 
 # Estabilsh connection
 conn_nwdb = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password)
-print(conn_nwdb)
+#print(conn_nwdb)
 
 
 
 
 # Create a curser
+# Allows us to executre read only queries
+
+cursor = conn_nwdb.cursor()
+
+cursor.execute("SELECT * FROM CUSTOMERS;")
+
+# using.execute() on a cursor
+#print(cursor)
+
+
+# print("////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
+
+# Fetch all .fetchall()
+# This is bad MMk'kay
+# We dont use this MM'kay...
+#rows = cursor.execute("SELECT * FROM CUSTOMERS").fetchall()
+
+#)print(rows)
+#print(len(rows))
+
+# rows = cursor.execute("SELECT * FROM PRODUCTS").fetchall()
+
+# for record in rows:
+#     print(type(record))
+#     print(record.UnitPrice) # We can access the colomn of a specific record
+
+# However, this is dangerous. we can clog our machine with too much data
+# We can use while loops to be more effcient
+
+rows  = cursor.execute("SELECT * FROM PRODUCTS")
+
+while True:
+    record = rows.fetchone()
+    if record is None:
+        break
+    print(record.UnitPrice)
+
+# Fetch
+
